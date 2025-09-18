b442b99ee3320e55967be75a210f72fb4fa6faac
APAC\DOR5HC <Nghia.DoTrong@vn.bosch.com>
2025-09-18 17:33:11 +0700

    fix hardcoded redirect url

--- Frontend/src/auth/authConfig.ts
+++ Frontend/src/auth/authConfig.ts
@@ -4,7 +4,7 @@ export const msalConfig: Configuration = {
   auth: {
     clientId: "7bde6a2e-9cbe-4348-85b3-a4723bcfff70",
     authority: "https://login.microsoftonline.com/0ae51e19-07c8-4e4b-bb6d-648ee58410f4",
-    redirectUri: "http://localhost:3000", // match Azure portal
+    redirectUri: window.location.origin, // match Azure portal
   },
   cache: {
     cacheLocation: "localStorage",
